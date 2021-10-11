21a54ed109cb5af22291b90c38ecd1d1f284ad54
TamirisR <Tamis_rodrigues@hotmail.com>
2021-10-10 21:51:41 -0300

    tela de produtos

--- app/src/main/java/br/senac/pi4pokemon/BottomNavigation.kt
+++ app/src/main/java/br/senac/pi4pokemon/BottomNavigation.kt
@@ -2,10 +2,17 @@ package br.senac.pi4pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.senac.pi4pokemon.databinding.ActivityBottomNavigationBinding
 
 class BottomNavigation : AppCompatActivity() {
+
+    lateinit var binding: ActivityBottomNavigationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bottom_navigation)
+        binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.itemIconTintList = null
     }
 }
---
+++ app/src/main/java/br/senac/pi4pokemon/ProductViewActivity.kt
@@ -0,0 +1,11 @@
+package br.senac.pi4pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_view)
+    }
+}
