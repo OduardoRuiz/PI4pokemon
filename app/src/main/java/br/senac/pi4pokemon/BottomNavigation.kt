5a9b7d82dbf3800eb98fdd025902df14fbf71fd9
OduardoRuiz <carloseduardoruizs@gmail.com>
2021-10-20 20:23:53 -0300

    Implementação fragmentos

--- app/src/main/java/br/senac/pi4pokemon/BottomNavigation.kt
+++ app/src/main/java/br/senac/pi4pokemon/BottomNavigation.kt
@@ -2,7 +2,9 @@ package br.senac.pi4pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import br.senac.pi4pokemon.databinding.ActivityBottomNavigationBinding
+import br.senac.pi4pokemon.fragments.*
 
 class BottomNavigation : AppCompatActivity() {
 
@@ -10,9 +12,44 @@ class BottomNavigation : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+
+
+
+
+
         super.onCreate(savedInstanceState)
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.bottomNavigationView.itemIconTintList = null
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.categorias -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, categoriasFragment()).commit()
+                }
+                R.id.lendarios -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, lendariosFragment()).commit()
+                }
+                R.id.meucarrinho -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, meuCarrinhoFragment()).commit()
+                }
+                R.id.minhaconta -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, minhaContaFragment()).commit()
+                }
+
+                R.id.inicio -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, inicioFragment()).commit()
+                }
+
+            }
+
+            true
+        }
     }
 }
