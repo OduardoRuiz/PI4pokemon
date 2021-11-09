35fba0d75f02694bdd39600e8a8ab2a20fc2bdbe
OduardoRuiz <carloseduardoruizs@gmail.com>
2021-11-08 21:11:17 -0300

    Tela Confirmação pedido, pedido confirmado

---
+++ app/src/main/java/br/senac/pi4pokemon/views/ConfirmacaoActivity.kt
@@ -0,0 +1,12 @@
+package br.senac.pi4pokemon.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senac.pi4pokemon.R
+
+class ConfirmacaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmacao)
+    }
+}
---
+++ app/src/main/java/br/senac/pi4pokemon/views/PedidoConfirmadoActivity.kt
@@ -0,0 +1,12 @@
+package br.senac.pi4pokemon.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senac.pi4pokemon.R
+
+class PedidoConfirmadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pedido_confirmado)
+    }
+}
